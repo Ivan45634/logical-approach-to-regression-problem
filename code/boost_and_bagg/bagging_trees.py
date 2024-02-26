11047b30bd38e73b6fe5309c1f1b2485c514c8b2
Ivan Listopadov <67547747+Ivan45634@users.noreply.github.com>
2024-02-27 00:28:16 +0300

    Create bagging_trees.py

---
+++ code/boost_and_bagg/bagging_trees.py
@@ -0,0 +1,54 @@
+import time
+import math
+import itertools
+import numpy as np
+from sklearn.metrics import mean_squared_error
+from joblib import Parallel, delayed
+from own_forest import *
+
+class BaggingElementaryTrees:
+    def __init__(self, base_estimator=None, n_estimators=10, sub_sample_size=1.0, sub_feature_size=1.0, bootstrap=True, bootstrap_features=True, n_jobs=None, random_state=None):
+        self.base_estimator = base_estimator if base_estimator is not None else BoostingElementaryPredicates()
+        self.n_estimators = n_estimators
+        self.sub_sample_size = sub_sample_size
+        self.sub_feature_size = sub_feature_size
+        self.bootstrap = bootstrap
+        self.bootstrap_features = bootstrap_features
+        self.n_jobs = n_jobs
+        self.random_state = random_state
+
+    def fit(self, X, y):
+        self.random_state_ = np.random.RandomState(self.random_state)
+        seeds = self.random_state_.randint(np.iinfo(np.int32).max, size=self.n_estimators)
+
+        self.estimators_ = []
+        self.estimators_features_ = []
+
+        if self.n_jobs is None:
+            for seed in seeds:
+                estimator, features = self._fit_estimator(X, y, seed)
+                self.estimators_.append(estimator)
+                self.estimators_features_.append(features)
+        else:
+            results = Parallel(n_jobs=self.n_jobs)(delayed(self._fit_estimator)(X, y, seed) for seed in seeds)
+            self.estimators_, self.estimators_features_ = zip(*results)
+
+        return self
+
+    def _fit_estimator(self, X, y, seed):
+        rng = np.random.default_rng(seed)
+        samples = _generate_bagging_indices(rng, X.shape[0], self.sub_sample_size, self.bootstrap)
+        if self.bootstrap_features:
+            features = _generate_bagging_indices(rng, X.shape[1], self.sub_feature_size, self.bootstrap)
+        else:
+            features = np.arange(X.shape[1])
+
+        estimator = self.base_estimator.__class__()  
+        estimator.fit(X[samples][:, features], y[samples])
+        
+        return estimator, features
+
+    def predict(self, X):
+        # Соберите предсказания от всех деревьев
+        predictions = np.mean([est.predict(X[:, features]) for est, features in zip(self.estimators_, self.estimators_features_)], axis=0)
+        return predictions
